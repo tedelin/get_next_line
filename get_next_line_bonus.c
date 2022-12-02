82c9adea4a6a0e3213155435fbd87ea60981049f
Tanguy EDELIN <tedelin@e2r12p2.clusters.42paris.fr>
2022-12-02 20:13:27 +0100

    add bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tedelin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 13:14:40 by tedelin           #+#    #+#             */
+/*   Updated: 2022/11/24 17:03:12 by tedelin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_checkstash(char **stash)
+{
+	char	*line;
+	int		i;
+
+	if ((*stash) && ft_strchr((*stash), '\n'))
+	{
+		i = 0;
+		line = malloc(sizeof(char) * (ft_alloc((*stash)) + 1));
+		if (!line)
+			return (NULL);
+		while ((*stash)[i] && (*stash)[i] != '\n')
+		{
+			line[i] = (*stash)[i];
+			i++;
+		}
+		line[i] = (*stash)[i];
+		if ((*stash)[i] != '0')
+			line[++i] = '\0';
+		(*stash) = ft_strdup(&(*stash)[i], (*stash));
+		return (line);
+	}
+	return (NULL);
+}
+
+int	ft_read(int fd, char **buff)
+{
+	int	b_read;
+
+	(*buff) = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!(*buff))
+		return (0);
+	b_read = read(fd, (*buff), BUFFER_SIZE);
+	if (b_read <= 0)
+		return (free((*buff)), 0);
+	(*buff)[b_read] = 0;
+	return (b_read);
+}
+
+char	*ft_line(char **buff, char **stash)
+{
+	int		i;
+	char	*line;
+
+	if (!(*buff))
+		return (NULL);
+	i = 0;
+	line = malloc(sizeof(char) * (ft_alloc((*buff)) + 1));
+	if (!line)
+		return (NULL);
+	while ((*buff)[i] && (*buff)[i] != '\n')
+	{
+		line[i] = (*buff)[i];
+		i++;
+	}
+	line[i] = (*buff)[i];
+	if ((*buff)[i] != '\0')
+		line[++i] = '\0';
+	line = ft_strjoin((*stash), line);
+	(*stash) = ft_strdup(&(*buff)[i], (*buff));
+	return (line);
+}
+
+char	*ft_lastline(char **stash)
+{
+	char	*line;
+
+	line = ft_strdup((*stash), (*stash));
+	(*stash) = NULL;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash[1024];
+	char		*line;
+	char		*buff;
+	int			b_read;
+
+	if (fd == -1)
+		return (NULL);
+	line = ft_checkstash(&stash[fd]);
+	if (line)
+		return (line);
+	b_read = ft_read(fd, &buff);
+	if (stash[fd] && b_read == 0 && !line)
+		return (ft_lastline(&stash[fd]));
+	if (!line && b_read > 0)
+	{
+		if (ft_strchr(buff, '\n'))
+		{
+			line = ft_line(&buff, &stash[fd]);
+			return (line);
+		}
+		stash[fd] = ft_strjoin(stash[fd], buff);
+		return (get_next_line(fd));
+	}
+	return (NULL);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tedelin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 16:32:00 by tedelin           #+#    #+#             */
+/*   Updated: 2022/11/20 16:32:03 by tedelin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stddef.h>
+
+char	*ft_checkstash(char **stash);
+int		ft_read(int fd, char **buff);
+char	*ft_line(char **buff, char **stash);
+char	*ft_lastline(char **stash);
+size_t	ft_strlen(const char *s);
+int		ft_alloc(char *buff);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s, char *f);
+char	*get_next_line(int fd);
+
+#endif
